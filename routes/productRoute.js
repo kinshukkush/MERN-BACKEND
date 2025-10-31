560214a1c23df27a7da111da453cb8bce311db5e
kinshukkush <kinshuksaxena3@gmail.com>
2025-10-31 19:45:54 +0530

    Add admin authentication to product routes

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { authenticate, authorize } from "../middlewares/auth.js";
 import { addProduct,showProducts,deleteProduct,updateProduct,getProduct,displayProducts } from "../controllers/productController.js";
 
 const Router = express.Router();
@@ -7,11 +8,11 @@ const Router = express.Router();
 //user 
 Router.get("/all", displayProducts);
 
-//admin
-Router.get("/", showProducts);
-Router.post("/", addProduct);
-Router.get("/:id", getProduct);
-Router.patch("/:id", updateProduct);
-Router.delete("/:id", deleteProduct);
+//admin - protected routes
+Router.get("/", authenticate, authorize("admin"), showProducts);
+Router.post("/", authenticate, authorize("admin"), addProduct);
+Router.get("/:id", authenticate, authorize("admin"), getProduct);
+Router.patch("/:id", authenticate, authorize("admin"), updateProduct);
+Router.delete("/:id", authenticate, authorize("admin"), deleteProduct);
 
 export default Router;
